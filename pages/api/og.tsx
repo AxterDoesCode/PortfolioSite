a629c7999cea6e81b5b6a99eac5d38c100282b78
axtr <chaualex88@hotmail.com>
2023-08-22 20:41:32 +0100

    Updated font for OG page

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,12 +1,12 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
-import { mocha as cp} from '../../public/colors/mocha'
+import { mocha as cp } from '../../public/colors/mocha'
 
 export const config = {
     runtime: 'edge',
 };
 
-export default function handler(request: NextRequest) {
+export default async function handler(request: NextRequest) {
     try {
         const { searchParams } = new URL(request.url);
 
@@ -16,6 +16,9 @@ export default function handler(request: NextRequest) {
             ? searchParams.get('title')?.slice(0, 100)
             : "Alex Chau's blog";
 
+        const fontData = await fetch(
+            new URL('../../public/fonts/JetBrainsMonoNerdFont-Bold.ttf', import.meta.url),
+        ).then((res) => res.arrayBuffer());
         return new ImageResponse(
             (
                 <div
@@ -32,10 +35,11 @@ export default function handler(request: NextRequest) {
                         flexWrap: 'nowrap',
                     }}
                 >
-                    <div
+                    <h1
                         style={{
                             fontSize: "5rem",
-                            fontStyle: 'bold',
+                            fontFamily: '"JetBrainsMonoNerd"',
+                            fontWeight: '700',
                             letterSpacing: '-0.025em',
                             color: cp.Text,
                             padding: '0 120px',
@@ -44,12 +48,19 @@ export default function handler(request: NextRequest) {
                         }}
                     >
                         {title}
-                    </div>
+                    </h1>
                 </div>
             ),
             {
                 width: 1200,
                 height: 630,
+                fonts: [
+                    {
+                        name: 'JetBrainsMonoNerd',
+                        data: fontData,
+                        style: 'normal',
+                    },
+                ],
             },
         );
     } catch (e: any) {
