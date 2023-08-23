199e015d47f96439c3551f6488b3f54872dc6f3d
axtr <chaualex88@hotmail.com>
2023-08-23 12:36:24 +0100

    New bg again optimised for longer text

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -25,7 +25,7 @@ export default async function handler(request: NextRequest) {
                 <div
                     style={{
                         backgroundSize: '1200px 630px',
-                        backgroundImage: `url(https://github.com/AxterDoesCode/PortfolioSite/blob/master/public/images/bg.png?raw=true)`,
+                        backgroundImage: `url(https://github.com/AxterDoesCode/PortfolioSite/blob/master/public/images/bg2.png?raw=true)`,
                         height: '100%',
                         width: '100%',
                         display: 'flex',
@@ -44,7 +44,7 @@ export default async function handler(request: NextRequest) {
                             height: '70%',
                             alignItems: 'flex-start',
                             justifyContent: 'flex-start',
-                            padding: '0 50px 0 24px',
+                            padding: '50px 50px 0 24px',
                             textAlign: 'left'
                         }}
                     >
